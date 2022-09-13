2a05aebf34a4589a38f04a8dfe4bd559b497c6c7
chester <chester0148@gmail.com>
2022-09-13 13:55:07 +0800

    add: 新增sql 範例

---
+++ webapi/sqlscript/PostgreSQL範例.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [IF NOT EXISTS] SampleTable (
+  `id` INT UNSIGNED AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `author` VARCHAR(40) NOT NULL,
+  `createDate` DATE,
+  PRIMARY KEY ( `id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+comment on column SampleTable.title is '標題';
+comment on column SampleTable.author is '作者';
